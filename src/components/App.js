a89525b74cc6996ca95e0b9a802a6c45c909997e
Shahin Azizi <hawk.azizi1@gmail.com>
2022-12-22 02:03:30 +0330

    create to-do list app

--- src/components/App.js
+++ src/components/App.js
@@ -1,78 +1,37 @@
 import React, { useState } from "react";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: "",
-  });
-  const [isOver, setIsOver] = useState(false);
-
-  function changeHandler(event) {
-    const { name, value } = event.target;
-
-    setContact((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email,
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email,
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value,
-        };
-      }
+  const [inputItems, setInputItems] = useState("");
+  const [items, setItems] = useState(["enter a value"]);
+  const changeHandler = (event) => {
+    const value = event.target.value;
+    setInputItems(value);
+  };
+  const clickHandler = (e) => {
+    e.preventDefault();
+    setItems((prevItems) => {
+      return [inputItems, ...prevItems];
     });
-  }
-  const mouseOverHandler = () => {
-    setIsOver(true);
+    setInputItems("");
   };
-  const mouseOutHandler = () => {
-    setIsOver(false);
-  };
-
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          onChange={changeHandler}
-          value={contact.fName}
-          name="fName"
-          placeholder="First Name"
-        />
-        <input
-          onChange={changeHandler}
-          value={contact.lName}
-          name="lName"
-          placeholder="Last Name"
-        />
-        <input
-          onChange={changeHandler}
-          value={contact.email}
-          name="email"
-          placeholder="Email"
-        />
-        <button
-          onMouseOver={mouseOverHandler}
-          onMouseOut={mouseOutHandler}
-          style={{ backgroundColor: isOver ? "black" : "white" }}
-        >
-          Submit
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={changeHandler} type="text" value={inputItems} />
+        <button onClick={clickHandler}>
+          <span>Add</span>
         </button>
-      </form>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
